4ae30117ee47a31ed135b309b306c547cea4124e
Austin <baik.austin@gmail.com>
2022-04-17 10:21:44 -0700

    commit tree 00882521ef6f30c90c00bed48c30978c7f5724ab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,8 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
   const linksElements = links.map((link)=> { 
-     return <a key={link} href=`#{link}`>{link}</a>
+    console.log('link', link)
+     return <a key={link} href={'#'+link}>{link}</a>
   });
   return (
     <nav>
